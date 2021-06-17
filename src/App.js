5e78d9938898751c12274542b950f7fe68b0e675
Halleys-Comet <jhalley230@gmail.com>
2021-06-16 20:50:14 -0500

    create components

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import Home from './components/Home';
+import About from './components/About';
+import Nav from './components/Nav';
+import Projects from './components/Projects';
+import Resume from './components/Resume';
+import Contact from './components/Contact';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+    <Nav></Nav>
+    <Switch>
+      <Route component={Home} path='/' exact />
+      <Route component={About} path='/about' />
+      <Route component={Projects} path='/projects' />
+      <Route component={Contact} path='/contact' />
+      <Route component={Resume} path='/resume' />
+    </Switch>
+    </BrowserRouter>
   );
 }
 
